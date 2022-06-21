9fbcbe91e153586f66c607f6005a75245c9d9c1e
teeny909 <sk8leo@live.com>
2022-06-21 15:25:18 -0400

    complete login functionality with request helper

--- src/main/java/com/revature/web/FrontController.java
+++ src/main/java/com/revature/web/FrontController.java
@@ -6,33 +6,47 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class FrontController
- */
 public class FrontController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FrontController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	/*
+	 * this method will be responsible for determining what resource the client is
+	 * requesting
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 1. URI rewriting
+		// http://localhost:8080/employee-servlet-app/login -- want to capture login
+		// http://localhost:8080/employee-servlet-app/login --if they go here it returns
+		// all employees in the DB
+		final String URI = request.getRequestURI().replace("/employee-servlet-app/", "");// eliminate everyting but the
+																							// uri
+
+		// 2. set switch case statement in which we call the appropriate functionality
+		// based on the URI returned
+		// ie. login, register, depends on uri
+		switch (URI) {
+		case "login":
+			// invoke some function from the RequestHelper
+			RequestHelper.processLogin(request, response);//forwards request+response object to reqwuest helper which will do stuff w the requyest  
+			break;
+		case "employees":
+			// ivoke some functionality from the RequestHelper which would return all
+			// Employees
+			break;
+		default:
+			// custom error page in case someone put garbage in uri here
+			break;
+		}
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		doGet(request, response);
 	}
 
